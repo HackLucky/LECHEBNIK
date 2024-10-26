cce71b4d602d3cedc3bee5ad0bc1d9ef73866af2
HackLucky <spectrago67@mail.ru>
2024-10-26 19:44:59 +0300

    Update tables, add new queries.

---
+++ SQLQuery/SQLQueryFillingInTheSuppliersWithData.sql
@@ -0,0 +1,11 @@
+INSERT INTO Suppliers (id_supplier, suppliers_name, country, website, phone_number) VALUES
+(1, 'ФармаГрупп', 'Россия', 'www.pharmagroup.ru', '+7 (495) 123-45-67'),
+(2, 'Медика', 'США', 'www.medica.com', '+1 (800) 555-1234'),
+(3, 'ЗдравКор', 'Германия', 'www.zdravkor.de', '+49 30 12345678'),
+(4, 'ТехноФарм', 'Канада', 'www.technopharm.ca', '+1 (416) 555-6789'),
+(5, 'ФармТек', 'Великобритания', 'www.pharmatech.co.uk', '+44 20 7946 0958'),
+(6, 'РосФарм', 'Россия', 'www.rospharm.ru', '+7 (812) 555-01-23'),
+(7, 'ФармаСервис', 'США', 'www.pharmaservice.com', '+1 (202) 555-6789'),
+(8, 'Синтез', 'Италия', 'www.synthesis.it', '+39 06 12345678'),
+(9, 'Генерикум', 'Франция', 'www.genericum.fr', '+33 1 42 123456'),
+(10, 'АльфаФарм', 'Швейцария', 'www.alfapharm.ch', '+41 44 123 45 67');
---
+++ SQLQuery/SQLQueryUpdateTableProducts.sql
@@ -0,0 +1,5 @@
+UPDATE Products SET id_taking = 1, id_state = 2, id_type = 3 WHERE id_medicine BETWEEN 1 AND 20;
+UPDATE Products SET id_taking = 2, id_state = 3, id_type = 1 WHERE id_medicine BETWEEN 21 AND 40;
+UPDATE Products SET id_taking = 3, id_state = 1, id_type = 2 WHERE id_medicine BETWEEN 41 AND 60;
+UPDATE Products SET id_taking = 4, id_state = 2, id_type = 5 WHERE id_medicine BETWEEN 61 AND 80;
+UPDATE Products SET id_taking = 5, id_state = 3, id_type = 4 WHERE id_medicine BETWEEN 81 AND 100;
