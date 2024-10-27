2e209688396c4f3b8a5d7f6e11e77534a6bb46fb
HackLucky <spectrago67@mail.ru>
2024-10-27 14:54:35 +0300

    Update Profile, About forms

--- SokolovLechebnik/SokolovLechebnik/Windows/About.xaml.cs
+++ SokolovLechebnik/SokolovLechebnik/Windows/About.xaml.cs
@@ -1,27 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace SokolovLechebnik.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для About.xaml
-    /// </summary>
     public partial class About : Window
     {
+        private bool isLight = true;
+        private bool isClick = true;
         public About()
         {
             InitializeComponent();
         }
+        private void ButtonExit_Click(object sender, RoutedEventArgs e) => Close();
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPopup.IsOpen = !MenuPopup.IsOpen;
+            ToggleButtonImage(ButtonMenu, isClick ? "pack://application:,,,/Resources/LechebnikLogoMainX.png" : "pack://application:,,,/Resources/LechebnikLogoMain.png");
+            isClick = !isClick;
+        }
+        private void ButtonOnOff_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleTheme(isLight);
+            ToggleButtonImage(ButtonOnOff, isLight ? "pack://application:,,,/Resources/dark_mode.png" : "pack://application:,,,/Resources/light_mode.png");
+            isLight = !isLight;
+        }
+        private void ToggleTheme(bool isLightTheme)
+        {
+            GridMain.Background = new SolidColorBrush(Color.FromRgb(isLightTheme ? (byte)175 : (byte)25, isLightTheme ? (byte)175 : (byte)25, isLightTheme ? (byte)175 : (byte)25));
+        }
+        private void ToggleButtonImage(Button button, string imageUri)
+        {
+            button.Background = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(imageUri)),
+                Stretch = Stretch.Uniform
+            };
+        }
+        private void OpenWindowAndCloseCurrent<T>() where T : Window, new()
+        {
+            new T().Show();
+            Close();
+        }
+        private void ButtonMain_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Main>();
+        private void ButtonProfile_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Profile>();
+        private void ButtonCatalog_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Catalog>();
+        private void ButtonBasket_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Basket>();
+        private void ButtonDisease_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<DiseaseBook>();
     }
 }
--- SokolovLechebnik/SokolovLechebnik/Windows/Profile.xaml.cs
+++ SokolovLechebnik/SokolovLechebnik/Windows/Profile.xaml.cs
@@ -1,27 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace SokolovLechebnik.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для Profile.xaml
-    /// </summary>
     public partial class Profile : Window
     {
+        private bool isLight = true;
+        private bool isClick = true;
         public Profile()
         {
             InitializeComponent();
         }
+        private void ButtonExit_Click(object sender, RoutedEventArgs e) => Close();
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPopup.IsOpen = !MenuPopup.IsOpen;
+            ToggleButtonImage(ButtonMenu, isClick ? "pack://application:,,,/Resources/LechebnikLogoMainX.png" : "pack://application:,,,/Resources/LechebnikLogoMain.png");
+            isClick = !isClick;
+        }
+        private void ButtonOnOff_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleTheme(isLight);
+            ToggleButtonImage(ButtonOnOff, isLight ? "pack://application:,,,/Resources/dark_mode.png" : "pack://application:,,,/Resources/light_mode.png");
+            isLight = !isLight;
+        }
+        private void ToggleTheme(bool isLightTheme)
+        {
+            GridMain.Background = new SolidColorBrush(Color.FromRgb(isLightTheme ? (byte)175 : (byte)25, isLightTheme ? (byte)175 : (byte)25, isLightTheme ? (byte)175 : (byte)25));
+        }
+        private void ToggleButtonImage(Button button, string imageUri)
+        {
+            button.Background = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(imageUri)),
+                Stretch = Stretch.Uniform
+            };
+        }
+        private void OpenWindowAndCloseCurrent<T>() where T : Window, new()
+        {
+            new T().Show();
+            Close();
+        }
+        private void ButtonMain_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Main>();
+        private void ButtonCatalog_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Catalog>();
+        private void ButtonBasket_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<Basket>();
+        private void ButtonDisease_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<DiseaseBook>();
+        private void ButtonAbout_Click(object sender, RoutedEventArgs e) => OpenWindowAndCloseCurrent<About>();
     }
 }
